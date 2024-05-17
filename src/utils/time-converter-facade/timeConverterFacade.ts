3a20616453308b272848321932232371e28eec5e
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-05-17 08:00:01 -0300

    chore: add facade to use time converter util

---
+++ src/utils/time-converter-facade/timeConverterFacade.ts
@@ -0,0 +1,24 @@
+import { TimeConverter } from "../time-converter/timeConverter";
+
+export class TimeConverterProcessorFacade {
+  private timeConverter: TimeConverter;
+
+  constructor() {
+    this.timeConverter = new TimeConverter();
+  }
+
+  fromTimeToDecimal(time: string | undefined): number {
+    return this.timeConverter.fromTime(time).toDecimal();
+  }
+
+  fromDecimalToTime(decimal: number | undefined): string {
+    return this.timeConverter.fromDecimal(decimal).toTime();
+  }
+
+  fromDecimalToHoursMinutes(decimal: number | undefined): {
+    hours: number;
+    minutes: number;
+  } {
+    return this.timeConverter.fromDecimal(decimal).toHoursMinutes();
+  }
+}
